64ac1b5c2960ee1be3a866833c8ab6a58a887b55
Carlos Tirado <mariotiradotovar@outlook.com>
2025-02-10 23:08:14 -0500

    middleware

--- src/routers/gestionVehiculosRoutes.js
+++ src/routers/gestionVehiculosRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { getVehiculos, getConceptos, getProveedores, getCostos } = require("../controllers/gestionVehiculosController");
+const validacionDeParametros = require("../../middlewares/validationsMiddleware");
 
 const router = express.Router();
 
 // Definir rutas
-router.get("/vehiculos", getVehiculos);
-router.get("/conceptos", getConceptos);
-router.get("/proveedores", getProveedores);
-router.get("/costos", getCostos);
+router.get("/vehiculos",validacionDeParametros, getVehiculos);
+router.get("/conceptos",validacionDeParametros, getConceptos);
+router.get("/proveedores",validacionDeParametros, getProveedores);
+router.get("/costos",validacionDeParametros, getCostos);
 
 module.exports = router;
